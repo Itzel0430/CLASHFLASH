fed06e2de8bebfb88d90a56789e3181a0ca828be
Itzel <karenitzelv42221@gmail.com>
2023-11-08 17:29:25 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CLASH_FLASH"
+include(":app")
+ 
