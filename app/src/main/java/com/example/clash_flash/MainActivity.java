ad86bf2ab2f55bfd02801dce4dd1fd404abe687e
Itzel <karenitzelv42221@gmail.com>
2023-11-17 11:32:49 -0600

    VISTAS COMPLETAS

--- app/src/main/java/com/example/clash_flash/MainActivity.java
+++ app/src/main/java/com/example/clash_flash/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.clash_flash;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+
+}
 }
